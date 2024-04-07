035f71017ad1262345f7b91b03f5f1f08b947d28
Fl0w <52174534+LPSebi@users.noreply.github.com>
2024-04-07 15:39:44 +0200

    Add file upload functionality and create "uploads" directory in app.js

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import multer from 'multer'
+import fs from 'fs'
 
 const app = express()
 app.use(cors({ origin: '*' }))
 app.use(bodyParser.urlencoded({ extended: true }))
+// create "uploads"
+fs.mkdir('uploads', { recursive: true }, (err) => {
+    if (err) throw err
+})
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -18,12 +23,16 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage })
 
-app.post('/api/fileanalyse', upload.single('file'), (req, res) => {
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
     res.json({
         name: req.file.originalname,
         type: req.file.mimetype,
         size: req.file.size,
     })
+})
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: './' })
 })
 
 app.listen(3000, () => {
