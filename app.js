266c6a5203ed5ae9c4272ab556b3bb76d13c8b31
Fl0w <52174534+LPSebi@users.noreply.github.com>
2024-04-07 15:16:10 +0200

    .

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import multer from 'multer'
+
+const app = express()
+app.use(cors({ origin: '*' }))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    },
+})
+
+const upload = multer({ storage: storage })
+
+app.post('/', upload.single('file'), (req, res) => {
+    res.json({
+        name: req.file.originalname,
+        type: req.file.mimetype,
+        size: req.file.size,
+    })
+})
